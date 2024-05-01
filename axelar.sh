0823fff4462d312d5a1a259fb66c44c5e23ccdd4
babiiih <69004972+babiiih@users.noreply.github.com>
2024-05-01 14:09:24 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL=https://tm.axelar.lava.build/lava-referer-9854154e-1270-4c92-907b-4a9ccf07358f/"
 
 OUTPUT_DIR="logs"
 
